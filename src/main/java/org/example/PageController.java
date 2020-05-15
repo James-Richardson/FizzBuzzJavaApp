3d77f0ab1614f85bac3084554abb5006ca5c670b
James Richardson <james.richardson@M97645.local>
2020-05-15 17:00:15 +0100

    Adding pageNum and pageSize query parameters.

--- src/main/java/org/example/PageController.java
+++ src/main/java/org/example/PageController.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.pojo.FizzBuzzValue;
 import org.example.pojo.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,9 +21,16 @@ import static org.example.FizzBuzz.toFizzBuzzValue;
 @RestController
 public class PageController {
 
-    @GetMapping("/page")
-    public Page getPage() {
-        List<FizzBuzzValue> values = rangeClosed(1, 100).mapToObj(toFizzBuzzValue).collect(toList());
+    @GetMapping("/getPage")
+    public Page getPage(
+            @RequestParam(value = "pageNum", defaultValue = "0") int pageNum,
+            @RequestParam(value = "pageSize", defaultValue = "100") int pageSize) {
+        int startInclusive = 1 + (pageNum * pageSize);
+        int endInclusive = (1 + pageNum) * pageSize;
+        List<FizzBuzzValue> values
+                = rangeClosed(startInclusive, endInclusive)
+                .mapToObj(toFizzBuzzValue)
+                .collect(toList());
 
         return new Page(values);
     }
--- src/test/java/org/example/PageControllerTest.java
+++ src/test/java/org/example/PageControllerTest.java
@@ -14,10 +14,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class PageControllerTest {
 
     private final PageController pageController = new PageController();
-    
+
     @Test
-    public void getPageReturnsFizzBuzzValuesFor1To100() {
-        Page page = pageController.getPage();
+    public void getPagePageNum0PageSize100ReturnsFizzBuzzValuesFor1To100() {
+        Page page = pageController.getPage(0, 100);
 
         List<FizzBuzzValue> values = page.getValues();
         assertNotNull(values);
@@ -26,6 +26,36 @@ class PageControllerTest {
         assertFalse(values.get(0).isFizz());
         assertFalse(values.get(0).isBuzz());
         assertEquals(100, values.get(99).getInteger());
+        assertFalse(values.get(99).isFizz());
+        assertTrue(values.get(99).isBuzz());
+    }
+
+    @Test
+    public void getPagePageNum1PageSize100ReturnsFizzBuzzValuesFor101To200() {
+        Page page = pageController.getPage(1, 100);
+
+        List<FizzBuzzValue> values = page.getValues();
+        assertNotNull(values);
+        assertEquals(100, values.size());
+        assertEquals(101, values.get(0).getInteger());
+        assertFalse(values.get(0).isFizz());
+        assertFalse(values.get(0).isBuzz());
+        assertEquals(200, values.get(99).getInteger());
+        assertFalse(values.get(99).isFizz());
+        assertTrue(values.get(99).isBuzz());
+    }
+
+    @Test
+    public void getPagePageNum1PageSize1000ReturnsFizzBuzzValuesFor1001To2000() {
+        Page page = pageController.getPage(1, 1000);
+
+        List<FizzBuzzValue> values = page.getValues();
+        assertNotNull(values);
+        assertEquals(1000, values.size());
+        assertEquals(1001, values.get(0).getInteger());
+        assertFalse(values.get(0).isFizz());
+        assertFalse(values.get(0).isBuzz());
+        assertEquals(2000, values.get(999).getInteger());
         assertFalse(values.get(99).isFizz());
         assertTrue(values.get(99).isBuzz());
     }
